44973ede4e1ad43801017d4f789df3428f021897
sebaek <sebaek@gmail.com>
2024-10-23 09:37:02 +0900

    현재 페이지 기준으로 왼쪽/오른쪽 끝페이지 구하기

--- src/main/java/com/example/prjjsp20241022/service/BoardService.java
+++ src/main/java/com/example/prjjsp20241022/service/BoardService.java
@@ -35,8 +35,16 @@ public class BoardService {
         // 페이지 관련 정보들
         Integer countAll = mapper.countAll();
         Integer lastPageNumber = (countAll - 1) / 10 + 1; // 마지막 페이지 번호
+        Integer rightPageNumber = ((page - 1) / 10 + 1) * 10; // 현재페이지 기준 오른쪽 끝 페이지 번호
+        Integer leftPageNumber = rightPageNumber - 9;// 현재페이지 기준 왼쪽 끝 페이지 번호
 
-        map.put("lastPageNumber", lastPageNumber);
+        Map<String, Object> pageInfo = new HashMap<>();
+
+        pageInfo.put("leftPageNumber", leftPageNumber);
+        pageInfo.put("rightPageNumber", rightPageNumber);
+        pageInfo.put("lastPageNumber", lastPageNumber);
+
+        map.put("pageInfo", pageInfo);
         map.put("boardList", list);
 
         return map;
--- src/main/webapp/WEB-INF/view/board/list.jsp
+++ src/main/webapp/WEB-INF/view/board/list.jsp
@@ -37,7 +37,9 @@
 
 <%--    pagination --%>
 <div>
-    <c:forEach begin="1" end="${lastPageNumber}" var="pageNumber">
+    <c:forEach begin="${pageInfo.leftPageNumber}"
+               end="${pageInfo.rightPageNumber}"
+               var="pageNumber">
         <a href="/board/list?page=${pageNumber}">${pageNumber}</a>
     </c:forEach>
 </div>
