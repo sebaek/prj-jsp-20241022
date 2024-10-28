3b78f817435ffa5bf8eab6b6bb79026e94409679
sebaek <sebaek@gmail.com>
2024-10-28 11:34:37 +0900

    게시물 목록에 작성자 별명 출력
    작성자 검색시 별명 검색

--- src/main/java/com/example/prjjsp20241022/mapper/BoardMapper.java
+++ src/main/java/com/example/prjjsp20241022/mapper/BoardMapper.java
@@ -54,8 +54,12 @@ public interface BoardMapper {
 
     @Select("""
             <script>
-                SELECT *
-                FROM board
+                SELECT b.id,
+                       b.title,
+                       b.inserted,
+                       m.nick_name writerNickName
+                FROM board b JOIN member m
+                    ON b.writer = m.id
                 <trim prefix="WHERE" prefixOverrides="OR">
                     <if test="searchTarget == 'all' or searchTarget == 'title'">
                         title LIKE CONCAT('%', #{keyword}, '%')
@@ -64,10 +68,10 @@ public interface BoardMapper {
                         OR content LIKE CONCAT('%', #{keyword}, '%')
                     </if>
                     <if test="searchTarget == 'all' or searchTarget == 'writer'">
-                        OR writer LIKE CONCAT('%', #{keyword}, '%')
+                        OR m.nick_name LIKE CONCAT('%', #{keyword}, '%')
                     </if>
                 </trim>
-                ORDER BY id DESC
+                ORDER BY b.id DESC
                 LIMIT #{offset}, 10
             </script>
             """)
@@ -75,7 +79,9 @@ public interface BoardMapper {
 
     @Select("""
             <script>
-                SELECT COUNT(id) FROM board
+                SELECT COUNT(b.id) 
+                FROM board b JOIN member m
+                    ON b.writer = m.id
                 <trim prefix="WHERE" prefixOverrides="OR">
                     <if test="searchTarget == 'all' or searchTarget == 'title'">
                         title LIKE CONCAT('%', #{keyword}, '%')
@@ -84,7 +90,7 @@ public interface BoardMapper {
                         OR content LIKE CONCAT('%', #{keyword}, '%')
                     </if>
                     <if test="searchTarget == 'all' or searchTarget == 'writer'">
-                        OR writer LIKE CONCAT('%', #{keyword}, '%')
+                        OR m.nick_name LIKE CONCAT('%', #{keyword}, '%')
                     </if>
                 </trim>
             </script>
